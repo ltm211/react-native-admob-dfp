809af88f365837911734261703a53ee587bed690
Fatih Tekcan <fmtekcan@hotmail.com>
2018-01-12 12:23:51 +0300

    implement DFPInterstitial

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ import AdMobBanner from './RNAdMobBanner';
 import AdMobInterstitial from './RNAdMobInterstitial';
 import PublisherBanner from './RNPublisherBanner';
 import AdMobRewarded from './RNAdMobRewarded';
+import DFPInterstitial from './RNDFPInterstitial';
 
-export { AdMobBanner, AdMobInterstitial, PublisherBanner, AdMobRewarded };
+
+export { AdMobBanner, AdMobInterstitial, PublisherBanner, AdMobRewarded, DFPInterstitial};
